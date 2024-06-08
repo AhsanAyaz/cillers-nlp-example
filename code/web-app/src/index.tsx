3d5d066ee205371ff140bfc026cdc59880cc67d6
Tania-RA <taniariazpk786@gmail.com>
2024-06-08 17:29:59 +0200

    Trying to run openai in graphql

--- code/web-app/src/index.tsx
+++ code/web-app/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { runOpenAi } from './api/openai';
+//import { runOpenAi } from './api/openai';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -19,4 +19,4 @@ root.render(
 );
 
 reportWebVitals();
-runOpenAi()
+//runOpenAi()
